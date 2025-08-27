19541c9a8713428da3bdabff9fe5b4719e93bf8f
Letícia <leticiacavalcanted.franca@gmail.com>
2025-08-27 18:37:51 -0300

    Add files via upload

---
+++ Lista1/ExercíciosPropostos/Q5/src/br/edu/principal/Principal.java
@@ -0,0 +1,13 @@
+package br.edu.principal;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		double lado = 5;
+		double area = lado * lado;
+		
+		System.out.println("Os lados do quadrados são: " + lado + " m.");
+		System.out.println("A área do quadrado é: " + area + " m².");
+	}
+
+}
