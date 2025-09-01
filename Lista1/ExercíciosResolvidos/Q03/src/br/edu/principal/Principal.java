33cb0654c5970a78a99adfc28833fc46d873e53e
Letícia França <leticiacavalcanted.franca@gmail.com>
2025-09-01 09:23:29 -0300

    Add files via upload

---
+++ Lista1/ExercíciosResolvidos/Q03/src/br/edu/principal/Principal.java
@@ -0,0 +1,37 @@
+package br.edu.principal;
+
+import java.util.Scanner;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner (System.in);
+		
+		double nota1, nota2, nota3;
+		double peso1, peso2, peso3;
+		double media;
+		
+		System.out.println ("Digite a primeira nota: ");
+		nota1 = sc.nextDouble();
+		
+		System.out.println ("Digite a segunda nota: ");
+		nota2 = sc.nextDouble();
+		
+		System.out.println ("Digite a terceira nota: ");
+		nota3 = sc.nextDouble();
+		
+		System.out.println ("Digite o primeiro peso: ");
+		peso1 = sc.nextDouble();
+		
+		System.out.println ("Digite o segundo peso: ");
+		peso2 = sc.nextDouble();
+		
+		System.out.println ("Digite o terceiro peso: ");
+		peso3 = sc.nextDouble();
+		
+		media = (nota1 * peso1 + nota2 * peso2 + nota3 * peso3) / (peso1 + peso2 + peso3);
+		
+		System.out.println ("A média é:" + media);
+	}
+
+}
